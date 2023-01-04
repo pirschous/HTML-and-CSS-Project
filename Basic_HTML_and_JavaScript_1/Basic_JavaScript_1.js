9c4a8a624bab21c9b3f8d74454b80e19a8aa2d48
pirschous <110510027+pirschous@users.noreply.github.com>
2023-01-03 19:03:27 -0800

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+alert("Hello, World! I am an alert box!");
+
+<script>
+	function My_First_Function() {
+		var String = "kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+</script>
